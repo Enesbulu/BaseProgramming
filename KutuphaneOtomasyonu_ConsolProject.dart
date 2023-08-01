fba5e64c2745b747613f3d301932aba1be4ace7f
Enes BULUT <bulut0258@gmail.com>
2023-08-01 10:41:11 +0300

    Complate.LastVersion

--- KutuphaneOtomasyonu_ConsolProject.dart
+++ KutuphaneOtomasyonu_ConsolProject.dart
@@ -9,19 +9,27 @@ void main() {
   bool durum = true;
 
   do {
+    Process.run("cls", [], runInShell: true);
     print(
         "\n\nE- Kişi Ekle\nL- Kişileri Listele\nS- Kişi Sil\nA- Kişi Ara\nT- Silinen Kişiler\nX- Programdan Çık");
     stdout.write("Yapmak İstediğiniz İşlemi Seçiniz: ");
     String? islem;
+    Process.run("cls", [], runInShell: true);
+
     try {
       islem = stdin.readLineSync()!;
-      if (islem.length > 0) {
+      if (islem.length > 1) {
         print("Hataı giriş yaptınız tekrar deneyiniz.");
         continue;
       }
     } catch (e) {
       print("Hata!!>>" + e.toString());
     }
+    if (islem == 'x') {
+      print("Çıkış yapıldı.");
+      break;
+    }
+    Process.run("cls", [], runInShell: true);
 
     switch (islem) {
       case 'e': //Kişi Ekleme
@@ -50,15 +58,18 @@ void main() {
         ConsolClear();
         do {
           //Kişi silme
-          for (int i = 0; i <= adSoyad.length; i++) {
-            print("#(i+1) - $adSoyad[i]");
+          for (int i = 0; i < adSoyad.length; i++) {
+            print("${i + 1}) - ${adSoyad[i]}");
           }
+          print("Bir üst menüye dönmek için (x)'e basın.");
           late int sira;
           stdout.write("Silmek istediğiniz kişinin numarasını giriniz:  ");
           do {
-            //dönüşün için hata kontrolü yapıldı.
+            //dönüşüm için hata kontrolü yapıldı.
+            String temp = stdin.readLineSync()!;
             try {
-              sira = int.parse(stdin.readLineSync()!);
+              if ('x' == temp) break;
+              sira = int.parse(temp);
             } catch (e) {
               print("Hatalı giriş yaptını tekrar giriniz!");
               continue;
@@ -67,7 +78,7 @@ void main() {
           } while (true);
 
           SilinenKisiler.add(adSoyad[sira - 1]);
-          adSoyad.remove(sira - 1);
+          adSoyad.removeAt(sira - 1);
           print("Kayıt Başarı ile silinmiştir.");
           stdout.write("Başka kayıt silmek istiyor musunuz?(e/h)");
           String istek = stdin.readLineSync()!;
@@ -98,7 +109,7 @@ void main() {
           }
           print("Başka bir arama yapmak ister misiniz? (e/h)");
           String temp = stdin.readLineSync()!;
-          if (temp == 'e') {
+          if (temp == 'h') {
             break;
           } else {
             continue;
@@ -109,6 +120,7 @@ void main() {
       case 't':
       case 'T': //Silinen Kişileri Listeleme
         ConsolClear();
+        if (SilinenKisiler.length == 0) break;
         print("Silinen Kişiler\n----------------------------------\n");
         SilinenKisiler.forEach((kisi) {
           print(kisi);
